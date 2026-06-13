0128e0f144cd374d09c03cdbf82e1ea8291b2f7a
shadowmind1337 <shadowmind1337@gmail.com>
2026-06-13 19:26:18 +0300

    Fix: add missing Sentry dependency and clean up main.tsx to fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useEffect } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import * as Sentry from "@sentry/react";
 import { 
